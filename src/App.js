0ecf58110e506832fa0bcc1f828cc9fef2e0b5bf
Can Taylan Tapar <cantaylantapar@gmail.com>
2021-07-23 23:28:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [baby, setBaby] = useState(null);
+  const [spouse, setSpouse] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const [gender, setGender] = useState("man");
+
+  const fetchAll = async (e) => {
+    e.preventDefault();
+    const babyUrl = "https://source.unsplash.com/featured/?baby,face";
+    const spouseUrl = `https://source.unsplash.com/featured/?${gender}`;
+
+    const babyResponse = await fetch(babyUrl);
+    const spouseResponse = await fetch(spouseUrl);
+
+    setBaby(babyResponse.url);
+    setSpouse(spouseResponse.url);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="container">
+        <form onSubmit={fetchAll}>
+          <div>Choose the gender of your spouse</div>
+          <div className="radio">
+            <label>
+              <input
+                type="radio"
+                value={gender}
+                checked={gender === "man"}
+                onChange={() => setGender("man")}
+              />
+              Man
+            </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input
+                type="radio"
+                value={gender}
+                checked={gender === "woman"}
+                onChange={() => setGender("woman")}
+              />
+              Woman
+            </label>
+          </div>
+          <button className="btn" type="submit">
+            show my future {gender === "woman" ? "wife" : "husband"} and child
+          </button>
+        </form>
+
+        <div className="gallery">
+          <div className="image-box first-image">
+            <img src={baby} alt="" />
+          </div>
+          <div className="image-box second-image">
+            <img src={spouse} alt="" />
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
-
 export default App;
