095b51c87965af5d20d5948f99d444941a70b513
Can Taylan Tapar <cantaylantapar@gmail.com>
2021-07-24 00:57:58 +0300

    responsive fixes and add spinner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Fragment, useState } from "react";
 import "./App.css";
+// import spinner from "./images/spinner.gif";
 
 function App() {
   const [baby, setBaby] = useState(null);
   const [spouse, setSpouse] = useState(null);
-  // const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [gender, setGender] = useState("man");
 
   const fetchAll = async (e) => {
     e.preventDefault();
+    setLoading(true);
     const babyUrl = "https://source.unsplash.com/featured/?baby,face";
     const spouseUrl = `https://source.unsplash.com/featured/?${gender}`;
 
@@ -17,37 +19,50 @@ function App() {
 
     setBaby(babyResponse.url);
     setSpouse(spouseResponse.url);
+    setLoading(false);
   };
 
   return (
     <Fragment>
       <div className="container">
         <form onSubmit={fetchAll}>
-          <div>Choose the gender of your spouse</div>
-          <div className="radio">
-            <label>
-              <input
-                type="radio"
-                value={gender}
-                checked={gender === "man"}
-                onChange={() => setGender("man")}
-              />
-              Man
-            </label>
-          </div>
-          <div className="radio">
-            <label>
-              <input
-                type="radio"
-                value={gender}
-                checked={gender === "woman"}
-                onChange={() => setGender("woman")}
-              />
-              Woman
-            </label>
+          <div className="wrapper">
+            <div className="text-box">Choose the gender of your spouse</div>
+            <div className="radio-btns">
+              <div className="radio">
+                <label>
+                  <input
+                    type="radio"
+                    value={gender}
+                    checked={gender === "man"}
+                    onChange={() => setGender("man")}
+                  />
+                  Man
+                </label>
+              </div>
+              <div className="radio">
+                <label>
+                  <input
+                    type="radio"
+                    value={gender}
+                    checked={gender === "woman"}
+                    onChange={() => setGender("woman")}
+                  />
+                  Woman
+                </label>
+              </div>
+            </div>
           </div>
           <button className="btn" type="submit">
-            show my future {gender === "woman" ? "wife" : "husband"} and child
+            {loading ? (
+              <div class="loader-container">
+                <div class="loader"></div>
+              </div>
+            ) : (
+              `show my future ${
+                gender === "woman" ? "wife" : "husband"
+              } and child`
+            )}
           </button>
         </form>
 
